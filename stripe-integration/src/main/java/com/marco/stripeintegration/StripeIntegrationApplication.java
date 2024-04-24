8c0a11e1c7ebe09aafe00ecf79bc07281f09d479
hermonAW <128448433+hermonAW@users.noreply.github.com>
2024-04-23 22:26:14 -0500

    Update StripeIntegrationApplication.java

--- stripe-integration/src/main/java/com/marco/stripeintegration/StripeIntegrationApplication.java
+++ stripe-integration/src/main/java/com/marco/stripeintegration/StripeIntegrationApplication.java
@@ -10,7 +10,7 @@ public class StripeIntegrationApplication {
 
     @PostConstruct
     public void setup() {
-        Stripe.apiKey = "sk_test_51MyKwgHIEsG8gydDyT938nQu9dzQ5A2PM57z6yPK4WTkJHW4SXkD9AGdBVj0Vfcb7pzrokPWgZ21yUmX9cZ1LFZz00eOttA60l";
+        Stripe.apiKey = "sk_test_";
     }
 
     public static void main(String[] args) {
